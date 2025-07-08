2a81aa8492141a6073100d1178ca57d1e37804d5
bwsq <thechillysq@gmail.com>
2025-07-08 20:16:58 +0800

    forgot to import datetime try again 3

--- application/auth_utils.py
+++ application/auth_utils.py
@@ -120,10 +120,20 @@ def log_admin_action(admin_user_id, action_desc):
 
     from datetime import datetime, timedelta
 
+from datetime import datetime, timedelta
+
 def convert_utc_to_gmt8_display(utc_datetime):
-    """Convert UTC datetime to GMT+8 for display purposes"""
-    if utc_datetime and isinstance(utc_datetime, datetime):
+    """Convert UTC datetime (string or datetime) to GMT+8 for display purposes"""
+    if isinstance(utc_datetime, str):
+        try:
+            # Try to parse the string to a datetime object (assuming UTC format)
+            utc_datetime = datetime.strptime(utc_datetime, '%Y-%m-%d %H:%M:%S')  # Adjust the format as needed
+        except ValueError:
+            return 'Invalid Date Format'
+    
+    if isinstance(utc_datetime, datetime):
         # Add 8 hours to convert UTC to GMT+8
         gmt8_datetime = utc_datetime + timedelta(hours=8)
         return gmt8_datetime.strftime('%Y-%m-%d %H:%M')
+    
     return str(utc_datetime) if utc_datetime else 'N/A'
